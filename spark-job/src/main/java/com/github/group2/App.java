c77fa7cbea5253912a85ea6bb37ec4b76c94a98d
pejalrath <pejalrath.077@gmail.com>
2020-05-20 16:45:31 -0700

    Add correlation of Number of Adult,Childrenn,and Baby & booking

--- spark-job/src/main/java/com/github/group2/App.java
+++ spark-job/src/main/java/com/github/group2/App.java
@@ -2,6 +2,12 @@ package com.github.group2;
 
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.functions;
+
+import scala.Tuple2;
+
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.sql.AnalysisException;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
@@ -14,11 +20,29 @@ public class App {
         spark.sparkContext().hadoopConfiguration().addResource("conf.xml");
         Dataset<Row> ds = spark.read().option("inferSchema", true).option("header", true)
                 .csv("s3a://revature-200413-project2-group2/hotel_bookings.csv").cache();
-        ds.printSchema();
-        ds.select("reservation_status_date").show(5);
-        ds = ds.withColumn("ID", functions.monotonically_increasing_id());
-        ds.createTempView("bookings");
-        spark.sql("SELECT ID, hotel, adults, children, reservation_status_date FROM bookings")
-                .show(10);
+
+        long num=ds.count();
+        JavaPairRDD <String,Integer> pair=method_1(ds);
+        for(Tuple2<String,Integer> s:pair.collect())
+        {
+        	System.out.printf(s+"  -> "+"%.4f",(s._2*100.0/num));
+        	System.out.println("%");
+        }
+        
     }
+    
+    public static JavaPairRDD<String,Integer> method_1(Dataset<Row> data)
+	{
+    	JavaRDD<Row> temp=data.javaRDD();
+		JavaPairRDD<String,Integer> result= temp.mapToPair(new PairFunction<Row,String,Integer>()
+				{
+					@Override
+					public Tuple2<String,Integer> call (Row input) throws Exception{
+						return new Tuple2<String,Integer>((input.get(9)+", "+input.get(10)+", "+input.get(11)),1);
+					}
+				}
+				);
+		result=result.reduceByKey((x,y)->x+y);
+		return result;
+	}
 }
