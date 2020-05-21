9e6331d2b403f5a95ea51465dc5d216dada1477c
Jay Patel <6jaypatel@gmail.com>
2020-05-21 08:04:09 -0400

    spark transformations

--- spark-job/src/main/java/com/github/group2/App.java
+++ spark-job/src/main/java/com/github/group2/App.java
@@ -2,6 +2,8 @@ package com.github.group2;
 
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.functions;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.AnalysisException;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
@@ -19,6 +21,17 @@ public class App {
         ds = ds.withColumn("ID", functions.monotonically_increasing_id());
         ds.createTempView("bookings");
         spark.sql("SELECT ID, hotel, adults, children, reservation_status_date FROM bookings")
-                .show(10);
+             .show(10);
+
+
+
+        // Correlation between hotel_type and arrival_date_month     
+        spark.sql("SELECT hotel, arrival_date_month, COUNT(hotel) FROM bookings group by hotel, arrival_date_month order by arrival_date_month").show(30);
+       
+        // Correlation between lead_time and hotel_type
+        spark.sql("SELECT hotel, COUNT(hotel), AVG(lead_time) FROM bookings group by hotel").show();
+
     }
+
+
 }
