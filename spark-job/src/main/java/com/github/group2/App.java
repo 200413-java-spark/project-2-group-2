e6708dc0dd455688c16666c104c528597fe9066c
JeffreyMagina <maginajeffrey@gmail.com>
2020-05-21 13:59:53 -0400

    main

--- spark-job/src/main/java/com/github/group2/App.java
+++ spark-job/src/main/java/com/github/group2/App.java
@@ -1,3 +1,9 @@
 package com.github.group2;
 
-
+public class App {
+    public static void main(String[] args) {
+        SimpleTransform simple = new SimpleTransform();
+        simple.schema();
+        simple.sqlstuff();
+    }
+}
