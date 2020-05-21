3d3d8f6248400908c0a828f3f5950c39223a54c8
JT <jtran0321@gmail.com>
2020-05-21 10:39:30 -0700

    refactor to use singleton pattern

---
+++ spark-job/src/main/java/com/github/group2/SessionCreator.java
@@ -0,0 +1,28 @@
+package com.github.group2;
+
+import org.apache.spark.sql.SparkSession;
+
+public class SessionCreator {
+  private static SparkSession spark;
+  private static SessionCreator instance;
+
+  private SessionCreator() {
+    // create SparkSession
+    if (spark == null) {
+      spark = new SparkSession.Builder().appName("EZ APP").master("local[*]").getOrCreate();
+      spark.sparkContext().setLogLevel("WARN");
+      spark.sparkContext().hadoopConfiguration().addResource("conf.xml");
+    }
+  }
+
+  public static SessionCreator getInstance() {
+    if (instance == null) {
+      instance = new SessionCreator();
+    }
+    return instance;
+  }
+
+  public SparkSession getSession() {
+    return spark;
+  }
+}
