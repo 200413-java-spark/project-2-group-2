2888ba6210e546c785f4d75f31d664caf5d19f01
pejalrath <pejalrath.077@gmail.com>
2020-05-22 08:17:52 -0700

    bug

--- spark-job/src/main/java/com/github/group2/SimpleTransform.java
+++ spark-job/src/main/java/com/github/group2/SimpleTransform.java
@@ -45,33 +45,5 @@ public class SimpleTransform {
 
   }
 
-  void compareHotelTypeAndMonth() {
-    // Correlation between hotel_type and arrival_date_month             
-    spark.sql("SELECT hotel, arrival_date_month, COUNT(hotel) FROM bookings group by hotel, arrival_date_month order by arrival_date_month").show(30);
-       
-  }
-
-  void compareAvgLeadTimeOfHotelType() {
-    // Correlation between lead_time and hotel_type
-    spark.sql("SELECT hotel, COUNT(hotel), AVG(lead_time) FROM bookings group by hotel").show();
-    
-  }
-
-  void compareAvgAdrOfRoomType() {
-    // Correlation between adr and room type
-    spark.sql("SELECT assigned_room_type, AVG(adr) FROM bookings group by assigned_room_type").show();
-  }
-  
-  void compareAvgAdrofMonth() {
-    // Correlation between adr and month
-    spark.sql("SELECT AVG(adr), arrival_date_month FROM bookings group by arrival_date_month").show();
-    
-  }
-
-  void compareAvgAdrofHotel() {
-    // Correlation between adr and hotel
-    spark.sql("SELECT AVG(adr), hotel FROM bookings group by hotel").show();
-    
-  }
 
 }
